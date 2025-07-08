b1089061f897619b6a98c3f3fe4ad2bce66f1cda
Md Raihanunnabi <you@example.com>
2025-07-08 22:58:28 +0600

    joking

---
+++ function/multiply_two_numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+        int multiply(int x,int y){
+           int sum;
+           sum=x*y;
+           return sum;
+        }
+int main(){
+ cout<<"this in the multiplication = "<<multiply(4,5)<<endl;
+
+return 0;
+
+}
