70ff1b31f94a3d1230c23f97b6254d5bc150e658
Panterahat <raverahat@gmail.com>
2025-08-11 14:06:28 +0600

    hello

--- arrays/function_to_sum_and_product_array.cpp
+++ arrays/function_to_sum_and_product_array.cpp
@@ -1,26 +1,29 @@
 #include <iostream>
 using namespace std;
-int main(){
-//input section
+int main()
+{
+    // input section
     int n;
-    cout<<"enter the total number= ";
-    cin>>n;
-int numbers[n];
-cout<<"enter the numbers"<<endl;
-for(int i=0;i<n;i++){
-cin>>numbers[i];
-}
-//sum
-int sum=0;
-for(int i=0;i<n;i++){
-sum=sum+numbers[i];
-}
-cout<<"sum is= "<<sum<<endl;
-//product
-int product=1;
-for(int i=0;i<n;i++){
-product=product*numbers[i];
-}
-cout<<"product is= "<<product<<endl;
-
+    cout << "enter the total number= ";
+    cin >> n;
+    int arr[n];
+    cout << "enter the numbers" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    // sum
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum = sum + arr[i];
+    }
+    cout << "sum is= " << sum << endl;
+    // product
+    int product = 1;
+    for (int i = 0; i < n; i++)
+    {
+        product = product * arr[i];
+    }
+    cout << "product is= " << product << endl;
 }
